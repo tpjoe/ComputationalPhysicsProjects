0d2ae44efbe7e7523c905697daf72f6b9e914a7c
Jialin <liujialin.buct@gmail.com>
2016-02-11 17:58:53 -0500

    Python delete boundary points

--- project1/Python/project1.py
+++ project1/Python/project1.py
@@ -10,7 +10,7 @@ fig1 = plt.figure()
 fig2 = plt.figure()
 
 # Define the test case
-testcase = (10, 100, 1000)
+testcase = (10, 10)
 print 'Gaussian Elimination'
 
 # Loop for different number of points
@@ -21,31 +21,28 @@ for m in testcase:
     d1 = 2
     e1 = -1
     h = 1 / (float(n+1) - 1)
-    d = d1 * np.ones((1, n+1))  # Diagonal
-    e = e1 * np.ones((1, n))  # Diagonal-1
-    x = (np.arange(n + 1.0)) / n  # x
+    d = d1 * np.ones((1, n-1))  # Diagonal
+    e = e1 * np.ones((1, n-2))  # Diagonal-1
+    x = (np.arange(n+1.0-2.0)) / n + h  # x
     f = (h ** 2.0) * 100.0 * np.exp(-10.0 * x)  # fi
     u = np.zeros((1, n+1))
 
     start = time.clock()                   # Start counting time
     # Forward Substitution
-    for i in xrange(1, n+1):
+    for i in xrange(1, n-1):
         d[0, i] = d[0, i] - e[0, i - 1] / d[0, i - 1] * e[0, i - 1]
         f[i] = f[i] - e[0, i - 1] / d[0, i - 1] * f[i - 1]
-
     # Backward Substitution
-    u[0, n] = f[n] / d[0, n]
-    for i in range(n-1, -1, -1):
+    u[0, n-2] = f[n-2] / d[0, n-2]
+    for i in range(n-3, -1, -1):
         u[0, i] = (f[i] - e[0, i] * u[0, i + 1]) / d[0, i]
     finish = time.clock()                  # End counting time
-
     # Exact solution and error
-    x = (np.arange(n + 1.0)) / n
-    x = x[range(1, n)]                     # Delete 0 in the denominator
-    u = u[0,range(1,n)]                    # Delete corresponding u
+    u = u[0,range(0,n-1)]                    # Delete corresponding u
     u_exact = 1-(1-np.exp(-10.0))*x-np.exp(-10.0*x)
-    u_exact.reshape((1, n-1))
-    e = np.log10((u-u_exact)/u_exact)      # error
+    e = np.log10(abs(u-u_exact)/u_exact)      # error
+    e1max = max(e)
+    print 'The maximum error from Gaussian elimination using', m, 'step is', e1max
 
     # Plot
     plt.figure(1)
@@ -67,26 +64,24 @@ plt.title('Relative error')
 print 'LU Decomposition'
 for m in testcase:
     # Generate the full matrix
-    A = np.diag(2*np.ones(m+1), 0) + np.diag(-1*np.ones(m), 1) + np.diag(-1*np.ones(m), -1)
+    A = np.diag(2*np.ones(m-1), 0) + np.diag(-1*np.ones(m-2), 1) + np.diag(-1*np.ones(m-2), -1)
+    #print A
     # Variables initialization
-    x = (np.arange(m + 1.0)) / m
     h = 1 / (float(m+1) - 1)
+    x = (np.arange(m + 1.0-2.0)) / m + h
     f = (h ** 2.0) * 100.0 * np.exp(-10.0 * x)  # fi
-
     # Solve using lib
     start = time.clock()
     LU = la.lu_factor(A)
     X = la.lu_solve(LU, f)
     finish = time.clock()
-
     # Calculate the errors
-    x = x[range(1, m)]                        # Delete 0 in denominator
-    X = X[range(1, m)]
+    # x = x[range(0, m-1)]                        # Delete 0 in denominator
+    X = X[range(0, m-1)]
     u_exact = 1-(1-np.exp(-10.0))*x-np.exp(-10.0*x)
-    u_exact.reshape((1, m+1))
-    u_exact = u_exact[1:m]
     e2 = np.log10(np.abs(X-u_exact)/u_exact)
-
+    e2max = max(e2)
+    print 'The maximum error from LU decomposition using', m, 'step is', e2max
     print 'time for LU is ', finish - start, 's'
 
     # Plot
@@ -99,4 +94,4 @@ for m in testcase:
 plt.figure(3)
 plt.plot(x, u_exact, label='exact')
 plt.legend()
-#plt.show()
+plt.show()
